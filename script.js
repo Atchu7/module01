c8019e6c1a38912f779b3cecbe0a72857ae8ba96
Atchu7 <85212240+Atchu7@users.noreply.github.com>
2021-06-23 20:16:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+Task 2:-
+
+1.Get all countries from Asia continent/region using filter function:-
+Code:-
+   var request= new XMLHttpRequest();
+   request.open('GET','https://restcountries.eu/rest/v2/all',true);
+   request.send();
+   request.onload = function(){
+   var data = JSON.parse(this.response);
+   var res=data.filter(function area(element){
+   return element.region==="Asia"});
+   console.log(res);
+   }
+
+2.Get all countries with population of less than 2 lacs using filter function:-
+Code:-
+  var request= new XMLHttpRequest();
+  request.open('GET','https://restcountries.eu/rest/v2/all',true);
+  request.send();
+  request.onload = function(){
+  var data = JSON.parse(this.response);
+  var result=data.filter(function pop(element){
+  return element.population<200000});
+  console.log(result);
+}
+
+3.Print the country which use US dollar as currency:-
+Code:-
+     var request= new XMLHttpRequest();
+     request.open('GET','https://restcountries.eu/rest/v2/all',true);
+     request.send();
+     request.onload = function(){
+     var data = JSON.parse(this.response);
+     var rest= data.filter(function curren(element){
+     return element.currencies[name]==="United States dollar";})
+     console.log(rest);
+    }
+
+
